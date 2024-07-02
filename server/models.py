d789ff549a32acaa158b9780ddaceb5f03c6ab0a
Brian <brian.m.muigai@gmail.com>
2024-07-02 22:15:49 +0300

    feat:added code for reviews

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import relationship
 
 
 metadata = MetaData(naming_convention={
@@ -10,12 +11,25 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+class Reviews(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = relationship('Customer', back_populates='customer_reviews')
+    item = relationship('Item', back_populates='item_reviews')
+
 
 class Customer(db.Model):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    customer_reviews = relationship('Reviews', back_populates='customer')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
@@ -27,6 +41,8 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
+
+    item_reviews = relationship('Reviews', back_populates='item')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
