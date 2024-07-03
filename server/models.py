5adc049f3961b80d87c7bea70bd4528d5f3b9db4
Brian <brian.m.muigai@gmail.com>
2024-07-03 09:35:23 +0300

    feat:completed lab

--- server/models.py
+++ server/models.py
@@ -4,45 +4,59 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import relationship
 
-
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
 
-class Reviews(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
-    comment = db.Column(db.String)
-    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
-    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    comment = db.Column(db.String, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=False)
 
-    customer = relationship('Customer', back_populates='customer_reviews')
-    item = relationship('Item', back_populates='item_reviews')
+    # Relationships
+    customer = relationship('Customer', back_populates='reviews')
+    item = relationship('Item', back_populates='reviews')
+
+    # Serialization rules
+    serialize_only = ('id', 'comment', 'customer_id', 'item_id')
+    exclude = ('customer.reviews', 'item.reviews')
+
+    def __repr__(self):
+        return f'<Review {self.id}, Customer {self.customer_id}, Item {self.item_id}>'
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
-    customer_reviews = relationship('Reviews', back_populates='customer')
+    serialize_only = ('id', 'name', 'reviews')  
+    exclude = ('reviews.customer',) 
+
+    reviews = relationship('Review', back_populates='customer')
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
-    item_reviews = relationship('Reviews', back_populates='item')
+    serialize_only = ('id', 'name', 'price', 'reviews')  
+    exclude = ('reviews.item',)
+
+    reviews = relationship('Review', back_populates='item')
 
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
